83503beb185cc19ba1a17c018f7cc026d0c9fe49
Jan Amann <jan@amann.me>
2017-07-08 01:27:41 +0200

    More consistent typing for FadeInRoute

--- src/FadeInRoute.js
+++ src/FadeInRoute.js
@@ -4,12 +4,11 @@ import {Transition} from 'react-transition-group';
 import {Route} from 'react-router-dom';
 import FadeIn from './FadeIn';
 
-const FadeInRoute = ({
-  component: Component,
-  ...rest
-}: {
+type Props = {
   component: ReactClass<*>
-}) =>
+};
+
+const FadeInRoute = ({component: Component, ...rest}: Props) =>
   <Route
     {...rest}
     children={({match, ...childProps}) =>
